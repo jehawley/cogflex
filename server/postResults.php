b7d4d6c1b12fb23c612d8543b843359f9feae1af
John Hawley <john.e.hawley@gmail.com>
2013-01-22 16:11:34 -0500

    cross

--- server/postResults.php
+++ server/postResults.php
@@ -1,5 +1,5 @@
 <?php
-  header('Access-Control-Allow-Origin: http://jehawley.github.com');
+  header('Access-Control-Allow-Origin: *');
   $participantData = $_POST['playDataString'];
   $fh = fopen('testData.csv', 'a+') or die('Error opening file');
   fwrite($fh, $participantData);
